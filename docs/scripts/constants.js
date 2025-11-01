4741b13c4849a91f593cdf6c42faed0aa4674e25
QA-Onrex <QA.Onrex@zohomail.eu>
2025-11-01 13:00:12 +0200

    Update constants.js

--- docs/scripts/constants.js
+++ docs/scripts/constants.js
@@ -2,10 +2,6 @@
 const CONFIG = {
     PASSWORD_HASH: '3718db2207be42cabda43cdfedb181ffef206cfda7ad775c7ba9e524104d2a32',
     DASHBOARD_DATA_URL: 'dashboard_data.json',
-    MAX_TOOLTIP_OFFSET: 10
+    MAX_TOOLTIP_OFFSET: 10,
+    TOOLTIP_PADDING: 10
 };
-
-// Export for use in other files
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { CONFIG };
-}
