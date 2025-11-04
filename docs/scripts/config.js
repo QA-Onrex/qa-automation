692e630a60ec323c20b133be97d863689e6123b1
QA-Onrex <QA.Onrex@zohomail.eu>
2025-11-04 11:48:27 +0200

    Create config.js

---
+++ docs/scripts/config.js
@@ -0,0 +1,9 @@
+// docs/scripts/config.js
+export const CONFIG = {
+    PASSWORD_HASH: '3718db2207be42cabda43cdfedb181ffef206cfda7ad775c7ba9e524104d2a32',
+    DASHBOARD_DATA_URL: 'dashboard_data.json',
+    ARCHIVE_INDEX_URL: 'archive/archive_index.json',
+    ARCHIVE_BASE_URL: 'archive/',
+    MAX_TOOLTIP_OFFSET: 10,
+    TOOLTIP_PADDING: 10
+};
